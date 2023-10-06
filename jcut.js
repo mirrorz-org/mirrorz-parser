0f4b47f75844fd923f0b15c3cc936df24fbcc550
BuildTools <unconfigured@null.spigotmc.org>
2023-10-06 20:31:33 +0800

    修改缩进格式

--- jcut.js
+++ jcut.js
@@ -1,12 +1,12 @@
 const tunasync = require("./tunasync");
 
 module.exports = async function (siteUrl) {
-	  const site = await (await fetch(siteUrl)).json();
-	  const mirrors = await tunasync("https://mirrors.jcut.edu.cn/jobs");
+  const site = await (await fetch(siteUrl)).json();
+  const mirrors = await tunasync("https://mirrors.jcut.edu.cn/jobs");
 
-	  return {
-		      site,
-		      info: [],
-		      mirrors,
-		    }
+  return {
+      site,
+      info: [],
+      mirrors,
+  }
 };
