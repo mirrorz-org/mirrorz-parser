17279614434d423a0a8f06857f3fe24b68e3d3e5
Palve <palve@nyist.edu.cn>
2024-03-06 18:30:19 +0800

    format

--- nyist.js
+++ nyist.js
@@ -5,7 +5,7 @@ const isoinfo = require("./isoinfo");
 module.exports = async function () {
   const site = await (await fetch("https://mirror.nyist.edu.cn/static/site.json")).json();
   let mirrors = await tunasync("https://mirror.nyist.edu.cn/static/tunasync.json");
-    mirrors = await options("https://mirror.nyist.edu.cn/static/js/options.json", mirrors);
+  mirrors = await options("https://mirror.nyist.edu.cn/static/js/options.json", mirrors);
   const info = await isoinfo("https://mirror.nyist.edu.cn/static/status/isoinfo.json");
   
   return {
