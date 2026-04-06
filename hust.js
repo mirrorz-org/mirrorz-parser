ed718152bcba508b60254e3d1843cde7550308cd
夜影星辰 <yeying-xingchen@openatom.club>
2026-04-06 20:11:20 +0800

    fix: var name mistake

--- hust.js
+++ hust.js
@@ -2,13 +2,13 @@ const tunasync = require("./tunasync");
 
 module.exports = async function (siteUrl) {
   const site = await (await fetch(siteUrl)).json();
-  const github-release = await (await fetch("https://gitee.com/hust-open-atom-club/mirrorrequest/raw/master/tunasync-scripts/github-release.json")).json();
+  const githubReleaseList = await (await fetch("https://gitee.com/hust-open-atom-club/mirrorrequest/raw/master/tunasync-scripts/github-release.json")).json();
   const mirrors = await tunasync("https://mirrors.hust.edu.cn/status.json");
 
   // github-release
   const githubRelease = mirrors.find(m => m.options_name === "github-release");
   if (githubRelease) {
-    github-release.forEach(item => {
+    githubReleaseList.forEach(item => {
       mirrors.push({
         cname: item.split('/').pop(),
         options_name: item.split('/').pop(),
