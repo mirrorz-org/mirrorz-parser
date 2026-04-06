0d4ddeae2acb9f865c8d025ecbc8184fdce0a66c
夜影星辰 <yeyingxingchen@yeah.net>
2026-04-06 13:22:15 +0800

    feat: add the info of github-release to mirrors.

--- hust.js
+++ hust.js
@@ -2,7 +2,21 @@ const tunasync = require("./tunasync");
 
 module.exports = async function (siteUrl) {
   const site = await (await fetch(siteUrl)).json();
+  const github-release = await (await fetch("https://github.com/hust-open-atom-club/mirrorrequest/blob/master/tunasync-scripts/github-release.json")).json();
   const mirrors = await tunasync("https://mirrors.hust.edu.cn/status.json");
+
+  // github-release
+  const githubRelease = mirrors.find(m => m.options_name === "github-release");
+  if (githubRelease) {
+    github-release.forEach(item => {
+      mirrors.push({
+        cname: item.split('/').pop(),
+        options_name: item.split('/').pop(),
+        url: item,
+        status: githubRelease.status
+      });
+    });
+  }
 
   return {
     site,
