0ec7af6a37f4bae5faebae80ebb085a1c8e33b17
Palvef <palve@nyist.edu.cn>
2026-07-13 18:02:52 +0800

    nyist: Fix parse path

--- nyist.js
+++ nyist.js
@@ -2,15 +2,20 @@ const tunasync = require("./tunasync");
 const options = require("./options");
 const isoinfo = require("./isoinfo");
 
-module.exports = async function () {
-  const site = await (await fetch("https://mirror.nyist.edu.cn/static/site.json")).json();
-  let mirrors = await tunasync("https://mirror.nyist.edu.cn/static/tunasync.json");
-  mirrors = await options("https://mirror.nyist.edu.cn/static/js/options.json", mirrors);
-  const info = await isoinfo("https://mirror.nyist.edu.cn/static/status/isoinfo.json");
-  
+module.exports = async function (siteUrl) {
+  const site = await (await fetch(siteUrl)).json();
+  let mirrors = await tunasync(
+    "https://mirror.nyist.edu.cn/static/tunasync.json",
+  );
+  mirrors = await options(
+    "https://mirror.nyist.edu.cn/static/options.json",
+    mirrors,
+  );
+  const info = await isoinfo("https://mirror.nyist.edu.cn/static/isoinfo.json");
+
   return {
     site,
     info,
     mirrors,
-  }
+  };
 };
